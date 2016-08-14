af24153ae3e6db3b4bdbbafe3f4bd84253054951
Jitendra Godha <jgodha@JJ-MBP.local>
2016-08-14 17:23:26 -0400

    Added tests/impl for more patterns to uncompact

--- romanNumeralCalculator.c
+++ romanNumeralCalculator.c
@@ -11,5 +11,10 @@ char* add(char *a, char *b) {
 
 char* uncompact(char *a) {
     if(strcmp(a, "IV") == 0) return "IIII";
+    if(strcmp(a, "IX") == 0) return "VIIII";
+    if(strcmp(a, "XL") == 0) return "XXXX";
+    if(strcmp(a, "XC") == 0) return "LXXXX";
+    if(strcmp(a, "CD") == 0) return "CCCC";
+    if(strcmp(a, "CM") == 0) return "DCCCC";
     return a;
 }
