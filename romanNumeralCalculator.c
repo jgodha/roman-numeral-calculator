57550dc4c1c1498d5ff2685f7d1b921bcb2e25de
Jitendra Godha <jgodha@JJ-MBP-2.local>
2016-08-17 22:36:56 -0400

    Refactoring and clean up

--- romanNumeralCalculator.c
+++ romanNumeralCalculator.c
@@ -2,7 +2,6 @@
 #include "string.h"
 #include "stringUtils.h"
 #include "romanNumeralCalculator.h"
-#include "stdio.h"
 
 /* TODO figure out how to declare these symbols in a Map-like structure */
 const char ROMAN[] = { 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
@@ -12,6 +11,19 @@ char* COMPACT_PATTERNS[] = { "IV", "IX", "XL", "XC", "CD", "CM" };
 char* UNCOMPACT_PATTERNS[] = { "IIII", "VIIII", "XXXX", "LXXXX", "CCCC", "DCCCC" };
 char* GROUPABLE_PATTERNS[] = { "IIIII", "VV", "XXXXX", "LL", "CCCCC", "DD" };
 char* GROUPS[] = { "V", "X", "L", "C", "D", "M" };
+
+char* add(char *a, char *b) {
+  char *result = sortByValueDescending(concatenate(uncompact(a), uncompact(b)));
+  return compact(group(result));
+}
+
+char* uncompact(char *a) {
+  int i;
+  for (i=0; i<6; i++) {
+    a = str_replace(a, COMPACT_PATTERNS[i], UNCOMPACT_PATTERNS[i]);
+  }
+  return a;
+}
 
 char* concatenate(char *a, char *b) {
     char *result = malloc(sizeof(a) + sizeof(b) + 1);
@@ -40,26 +52,18 @@ char* sortByValueDescending(char *a) {
     return a;
 }
 
-char* add(char *a, char *b) {
+char* group(char *a) {
   int i;
   for (i=0; i<6; i++) {
-    a = str_replace(a, COMPACT_PATTERNS[i], UNCOMPACT_PATTERNS[i]);
+    a = str_replace(a, GROUPABLE_PATTERNS[i], GROUPS[i]);
   }
+  return a;
+}
 
+char* compact(char *a) {
+  int i;
   for (i=0; i<6; i++) {
-    b = str_replace(b, COMPACT_PATTERNS[i], UNCOMPACT_PATTERNS[i]);
+    a = str_replace(a, UNCOMPACT_PATTERNS[i], COMPACT_PATTERNS[i]);
   }
-
-  char *result = concatenate(a, b);
-
-  result = sortByValueDescending(result);
-
-  for (i=0; i<6; i++) {
-    result = str_replace(result, GROUPABLE_PATTERNS[i], GROUPS[i]);
-  }
-
-  for (i=0; i<6; i++) {
-    result = str_replace(result, UNCOMPACT_PATTERNS[i], COMPACT_PATTERNS[i]);
-  }
-  return result;
+  return a;
 }
